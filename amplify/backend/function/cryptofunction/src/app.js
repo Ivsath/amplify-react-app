71a8dc74bb729aba78a85af4925acf2c5efd8133
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-30 13:09:54 -0600

    Add coins endpoint

--- amplify/backend/function/cryptofunction/src/app.js
+++ amplify/backend/function/cryptofunction/src/app.js
@@ -29,6 +29,16 @@ app.use(function (req, res, next) {
  * Example get method *
  **********************/
 
+app.get('/coins', function (req, res) {
+  const coins = [
+    { name: 'Bitcoin', symbol: 'BTC', price_usd: '10000' },
+    { name: 'Ethereum', symbol: 'ETH', price_usd: '400' },
+    { name: 'Litecoin', symbol: 'LTC', price_usd: '150' },
+  ]
+
+  res.json({ coins })
+})
+
 app.get('/item', function (req, res) {
   // Add your code here
   res.json({ success: 'get call succeed!', url: req.url })
